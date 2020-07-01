e775c78276ca108679645f7f312e031a3f34fa57
Nayara <nayara.lopes2007@gmail.com>
2020-07-01 15:50:18 -0300

    Added sass

--- links-frontend/src/index.js
+++ links-frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.scss';
 
 
 ReactDOM.render( < App / > , document.getElementById('root'));

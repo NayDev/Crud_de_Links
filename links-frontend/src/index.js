7cabe350273e99948405e9e2c5f9ce3b3929224c
Nayara <nayara.lopes2007@gmail.com>
2020-07-01 16:03:26 -0300

    Added UI Kit

--- links-frontend/src/index.js
+++ links-frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import './main.scss';
+import './styles/main.scss';
 
 
 ReactDOM.render( < App / > , document.getElementById('root'));

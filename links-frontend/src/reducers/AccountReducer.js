00b644c63ca45bffb25df8777b74459ef1eb7143
Nayara <nayara.lopes2007@gmail.com>
2020-07-02 21:12:40 -0300

    New approach to actions and reducers and logout function.

--- links-frontend/src/reducers/AccountReducer.js
+++ links-frontend/src/reducers/AccountReducer.js
@@ -1,10 +1,10 @@
-import {SIGN_IN} from './SignInActions';
-import { setAccount, setToken, setRefreshToken } from '../../helpers/account';
+import { SIGN_IN, SIGN_UP, SIGN_OUT  } from '../actions/AccountActions';
+import { setAccount, setToken, setRefreshToken, removeAccount, removeToken, removeRefreshToken } from '../helpers/account';
+
 
 const initialState = {
     account: null,
-    
-}
+};
 
 export default function (state = initialState, action) {
 
@@ -12,7 +12,7 @@ export default function (state = initialState, action) {
 
     switch(type) {
         case SIGN_IN:
-
+        case SIGN_UP:
             const response = payload ? payload.data : null;
             const account = response ? response.data : null;
             const metadata = response ? response.metadata : null;
@@ -20,13 +20,20 @@ export default function (state = initialState, action) {
             const token = metadata ? metadata.token : null;
             const refreshToken = metadata ? metadata.refreshToken : null;
 
-            
-
             if (account) setAccount(account);
             if (token) setToken(token);
             if (refreshToken) setRefreshToken(refreshToken);
-            
-            return {...initialState, account };
+
+            return {...state, account };
+
+            case SIGN_OUT: {
+
+                removeAccount();
+                removeToken();
+                removeRefreshToken();
+                return { ...initialState };
+            }
+
         default:
             return state;
     }
--- links-frontend/src/screens/Signup/index.js
+++ links-frontend/src/screens/Signup/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { signUp } from './SignUpActions';
+import { signUp } from '../../actions/AccountActions';
 
 const SignUp = (props) => {
 
@@ -52,7 +52,7 @@ const SignUp = (props) => {
     )
 }
 const mapStateToProps = (state) => {
-    return { account: state.signUp.account };
+    return { account: state.account.account };
 };
 
 export default connect(mapStateToProps, { signUp })(SignUp);
